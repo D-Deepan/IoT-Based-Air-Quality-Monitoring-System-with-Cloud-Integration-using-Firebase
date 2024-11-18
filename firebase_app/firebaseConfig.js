8cea73e588ffbef10f98be3013879d25f311ff99
Deepan <Deepandhandapani2005@gmail.com>
2024-11-18 07:39:28 +0530

    update firebaseConfig.js

--- firebase_app/firebaseConfig.js
+++ firebase_app/firebaseConfig.js
@@ -4,14 +4,15 @@ import { getDatabase, ref, onValue } from 'firebase/database';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDqtB-0bKHueXApd3YzK6Ach7dh85muJTo",
-  authDomain: "esp-firebase-720bc.firebaseapp.com",
-  databaseURL: "https://esp-firebase-720bc-default-rtdb.firebaseio.com",
-  projectId: "esp-firebase-720bc",
-  storageBucket: "esp-firebase-720bc.firebasestorage.app",
-  messagingSenderId: "746855134498",
-  appId: "1:746855134498:web:977bc4e744d32f42b6617b"
+  apiKey: "YOUR_API_KEY",                // Replace with your API Key
+  authDomain: "YOUR_AUTH_DOMAIN",        // Replace with your Auth Domain
+  databaseURL: "YOUR_DATABASE_URL",      // Replace with your Database URL
+  projectId: "YOUR_PROJECT_ID",          // Replace with your Project ID
+  storageBucket: "YOUR_STORAGE_BUCKET",  // Replace with your Storage Bucket
+  messagingSenderId: "YOUR_SENDER_ID",   // Replace with your Sender ID
+  appId: "YOUR_APP_ID"                   // Replace with your App ID
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
